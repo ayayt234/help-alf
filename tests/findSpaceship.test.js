516150043532ce2415dcc5af5f6414a5aa6b7442
Ayat Husein <ayayt234@gmail.com>
2026-05-22 17:38:00 +0200

    test: write failing test for spaceship at 0,0 (RED)

--- tests/findSpaceship.test.js
+++ tests/findSpaceship.test.js
@@ -1,5 +1,9 @@
 const findSpaceship = require('../src/findSpaceship');
 
 test('returns lost message for empty map', () => {
-  expect(findSpaceship('')).toBe('Spaceship lost forever.');
+    expect(findSpaceship('')).toBe('Spaceship lost forever.');
+});
+
+test('should return [0, 0] if the map only contains X', () => {
+    expect(findSpaceship('X')).toEqual([0, 0]);
 });
